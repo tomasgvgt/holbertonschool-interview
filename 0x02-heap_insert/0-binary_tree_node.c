4838290e7bea20740ee940d54261cba73de94ded
tomasgvgt <tomasgvg.t@gmail.com>
2021-08-06 10:00:09 -0500

    Betty

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -7,7 +7,7 @@
  *
  * @parent: Pointer to the parent node of the node to create
  * @value: Value to put in the new node
- * 
+ *
  * Return: node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
