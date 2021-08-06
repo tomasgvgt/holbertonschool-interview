3e432d67b68b7e38ffb505b83847de8f7022e2c2
tomasgvgt <tomasgvg.t@gmail.com>
2021-08-06 09:42:58 -0500

    Binary heap

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,17 +9,17 @@
  * @value: Value to put in the new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-    {
-        binary_tree_t *node;
+	{
+		binary_tree_t *node;
 
-        node = malloc(sizeof(binary_tree_t));
-        if (!node)
-            return (NULL);
+		node = malloc(sizeof(binary_tree_t));
+		if (!node)
+			return (NULL);
 
-        node->n = value;
-        node->parent = parent;
-        node->left = NULL;
-        node->right = NULL;
+		node->n = value;
+		node->parent = parent;
+		node->left = NULL;
+		node->right = NULL;
 
-        return (node);
-    }
+		return (node);
+	}
