325f15d79ecd06a00b88e55ee7b6b3ae1dff5288
tomasgvgt <tomasgvg.t@gmail.com>
2021-10-04 16:47:10 -0500

    -

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Write a script that reads stdin line by line and computes metrics:
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+    <status code> <file size>
+    (if the format is not this one, the line must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+    print these statistics from the beginning:
+
+    Total file size: File size: <total size>
+    where <total size> is the sum of all previous <file size>
+        (see input format above)
+    Number of lines by status code:
+        possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+        if a status code doesn’t appear or is not an integer,
+            don’t print anything for this status code
+        format: <status code>: <number>
+"""
+import sys
+
+file_size = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+
+try:
+    for i, line in enumerate(sys.stdin, 1):
+        splited = line.split(" ")
+        if len(splited) < 2:
+            continue
+        if splited[-2] in status_codes:
+            status_codes[splited[-2]] += 1
+        file_size += eval(splited[-1])
+        if i % 10 == 0:
+            print("File size: {}".format(file_size))
+            for key, value in sorted(status_codes.items()):
+                if value > 0:
+                    print("{}: {}".format(key, value))
+finally:
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
